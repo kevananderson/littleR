f809d10642e6ffd830756c7a135f34e74f7f56ab
Kevan Anderson <kevan_anderson@yahoo.com>
2024-11-09 17:54:59 -0800

    project is loaded at cwd level

--- littleR/manage.py
+++ littleR/manage.py
@@ -8,6 +8,7 @@ def cli():
         { 'fn':goodbye,                 'text': "Exit"},
         { 'fn':new_project,             'text': "Create New Project"},
         { 'fn':littleR_project,         'text': "Use the requirements for the littleR Project"},
+        { 'fn':remove_project,          'text': "Remove projects files"},
     ]
 
     while True:
@@ -41,30 +42,18 @@ def print_cli_options(selections):
     for index,selected in enumerate(selections):
         print(f"[{index+1}]: {selected['text']}")
 
-def make_doc_folder():
-    if not os.path.exists('doc'):
-        os.makedirs('doc')
+def can_copy_template():
+    if not os.path.exists('project') and \
+       not os.path.exists('reports') and \
+       not os.path.exists('config.yaml'):
+        return True
+    else:
+        print("Cannot overwrite existing project. To remove the the project, select Remove option from main.")
+
+    return False
 
 def template_path(template_name):
     return os.path.join(os.path.dirname(__file__), 'templates', template_name)
-
-def find_name_for_copy():
-    #TODO: we may want to have the project copied in the root directory
-    #our first choice is to call it littleR
-    path = 'doc/littleR'
-    if not os.path.exists('doc/littleR'):
-        return path
-    
-    #we need to append a number at the end
-    number = 1
-    while True:
-        path = f"doc/littleR_{number:02}"
-        if not os.path.exists(path):
-            return path
-        number += 1
-        if number >= 100:
-            raise ValueError("Tried 100 times but unable to find a valid dirctory name. Please check in the doc folder for too many littleRs.")
-            
 
 #*** Actions ***
 
@@ -72,18 +61,41 @@ def goodbye():
     print( "\nGoodbye." )
 
 def new_project():
-    #all setup is done in the doc folder
-    make_doc_folder()
+    #we are putting the template in the working directory
+    if can_copy_template():
 
-    #copy the template from littleR_req to the doc folder
-    shutil.copytree(template_path('new_req'), find_name_for_copy() )
+        #copy the template from littleR_req to the working dir
+        shutil.copytree(template_path('new_req'), os.getcwd(), dirs_exist_ok=True )
 
 def littleR_project():
-    #all setup is done in the doc folder
-    make_doc_folder()
+    #we are putting the template in the working directory
+    if can_copy_template():
 
-    #copy the template from littleR_req to the doc folder
-    shutil.copytree(template_path('littleR_req'), find_name_for_copy() )
+        #copy the template from littleR_req to the doc working dir
+        shutil.copytree(template_path('littleR_req'), os.getcwd(), dirs_exist_ok=True )
+
+def remove_project():
+    #does the project exist?
+    exists = os.path.exists('project') or os.path.exists('reports') or os.path.exists('config.yaml')
+
+    if exists:
+        #make sure the user can leave as needed
+        try:
+            print("!! Are you sure you want to delete the project? !!")
+            confirmation = input("Type [confirm] to delete files: ")
+        except KeyboardInterrupt:
+            goodbye()
+        
+        if confirmation == 'confirm':
+            if os.path.exists('project'):
+                shutil.rmtree('project', ignore_errors=True)
+            if os.path.exists('reports'):
+                shutil.rmtree('reports', ignore_errors=True)
+            if os.path.exists('config.yaml'):
+                os.remove('config.yaml')
+            print("Project files have been deleted.")
+        else:
+            print("Files NOT removed.")
 
 if __name__ == "__main__":
     cli()
